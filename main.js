f97002d562354fb0c687e8fbd5b0723983994a9a
Mahabubur Rahman <mahabuburr01625@gmail.com>
2020-12-06 10:45:01 +0600

    recursion

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
-function addition(a, b) {
-	return a + b
+function length(str) {
+	if (str === '') {
+		return 0;
+	} else {
+		 return length(str.substring(1)) + 1;
+	}
 }
+
+console.log(length('hello'));
